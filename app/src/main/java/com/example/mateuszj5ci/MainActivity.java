2f52af490b107f587849cf32be3fbee92eb68838
Migh4ty <mateo.1909@interia.pl>
2023-09-07 14:48:57 +0200

    pliki

---
+++ app/src/main/java/com/example/mateuszj5ci/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mateuszj5ci;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
